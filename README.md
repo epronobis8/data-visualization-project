de8d93ae18a1ce9aa0e10863bbb356d041acebf2
erin <pronobis_erin@bah.com>
2022-11-16 17:05:23 -0500

    updates to page 343

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 Required to install matplotlib.pyplot as part of this project.
 
 Steps:
-sudo python3 -m pip install -U pip
-sudo python3 -m pip install -U matplotlib
+1. sudo python3 -m pip install -U pip
+2. sudo python3 -m pip install -U matplotlib
--- sitka_highs_lows.py
+++ sitka_highs_lows.py
@@ -13,13 +13,15 @@ with open(filename) as f:
     #which containers the file headers
 
     # Get dates & high tempatures from this file.
-    dates, highs = [], []
+    dates, highs, lows = [], [], []
     for row in reader:
         current_date = datetime.strptime(row[2], '%Y-%m-%d')
         high = int(row[5])
+        low = int(row[6])
         dates.append(current_date)
         #pulling the data from index high which is the max temp column
         highs.append(high)
+        lows.append(low)
 
 print(highs)
 
@@ -27,10 +29,12 @@ print(highs)
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
 #plot the high values to red points
-ax.plot(dates, highs, c='red')
+ax.plot(dates, highs, c='red', alpha=0.5)
+ax.plot(dates, lows, c='blue', alpha=0.5)
+ax.fill_between(dates, high, lows, facecolor='blue', alpha=0.1)
 
 # Format Plot
-ax.set_title("Daily high tempatures - 2018", fontsize=24)
+ax.set_title("Daily high and low tempatures - 2018", fontsize=24)
 ax.set_xlabel('', fontsize=16)
 ax.set_ylabel("Tempature (F)", fontsize=16)
 fig.autofmt_xdate()
